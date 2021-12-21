bfe1d412c5b95937f70d10cce91432b28bca71c8
Володимир Єрмоленко <yermolim@gmail.com>
2021-12-21 20:06:43 +0200

    edit rollup config;

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,33 @@
+import dts from "rollup-plugin-dts";
 import license from "rollup-plugin-license";
 import externals from "rollup-plugin-node-externals";
-import dts from "rollup-plugin-dts";
 // import commonjs from "@rollup/plugin-commonjs";
 // import { nodeResolve } from "@rollup/plugin-node-resolve";
 // import { terser } from "rollup-plugin-terser";
+
+const banner = `
+MIT License
+
+Copyright (C) 2021-present Volodymyr Yermolenko (yermolim@gmail.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+`;
 
 export default [  
   // main build
@@ -17,29 +41,7 @@ export default [
     external: [],
     plugins: [
       license({
-        banner: `
-          MIT License
-
-          Copyright (C) 2021-present Volodymyr Yermolenko (yermolim@gmail.com)
-          
-          Permission is hereby granted, free of charge, to any person obtaining a copy
-          of this software and associated documentation files (the "Software"), to deal
-          in the Software without restriction, including without limitation the rights
-          to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-          copies of the Software, and to permit persons to whom the Software is
-          furnished to do so, subject to the following conditions:
-          
-          The above copyright notice and this permission notice shall be included in all
-          copies or substantial portions of the Software.
-          
-          THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-          IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-          FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-          AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-          LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-          OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-          SOFTWARE.
-        `,
+        banner,
       }),
       externals({
         deps: true,
@@ -47,6 +49,7 @@ export default [
       }),
     ]
   },
+  // types
   {
     input: "tsc/index.d.ts",
     output: [
